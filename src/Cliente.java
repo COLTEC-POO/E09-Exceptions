1fcc2cbf009f55751d28ade029d08157ff71196c
Jeferson Junio <jjsconstantino@gmail.com>
2022-11-03 10:02:46 -0300

    Finalização dos exercicios - E09-Exceptions - Jeferson Constantino

---
+++ src/Cliente.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+public abstract class Cliente {
+    // Atributos
+    private String nome;
+    private Date data;
+    private String endereco;
+
+    public Cliente() {
+
+    }
+
+    //Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    // Metodos
+
+    public void imprimirDadosCliente() {
+        System.out.println("Cliente invalido! ");
+    }
+
+    public abstract boolean autentica(String chave);
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cliente pessoa = new PessoaFisica("Josef", "Rua das Gracas",
+                "123.456.789-12", 21, 'm');
+        Conta cc = new ContaCorrente();
+        cc.setDono(pessoa);
+
+        try {
+            cc.setLimite(500);
+            cc.depositar(2000);
+            cc.sacar(5000);
+
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
+        } catch (ValorNegativoException e) {
+            System.out.println(e.getMessage());
+        } catch (SemLimiteException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
